a2314db00913a11ee0b7a77a8825dd2df6b43987
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-05-01 17:29:24 -0500

    feat: request component

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,16 @@ import {createRouter, createWebHistory} from 'vue-router';
 import LoginComponent from "../public/components/login.component.vue";
 import HomeComponent from "../public/components/home.component.vue";
 import RegisterCompanyComponent from "../public/components/register-company.component.vue";
+import Request from "../clients/components/request.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/login'},
         {path: '/login', component: LoginComponent, name: 'login'},
-        {path: '/home', component: HomeComponent, name: 'home'},
-        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'}
+        {path: '/home/:userId', component: HomeComponent, name: 'home',props:true},
+        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'},
+        {path: '/requests/:userId', component: Request, name: 'requests',props:true},
     ]
 });
 
