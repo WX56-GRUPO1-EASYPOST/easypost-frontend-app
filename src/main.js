9ed1c8aa7e51f78826adbc6c06b9750dba0a9cae
JorgeQuilla <u20211b197@upc.edu.pe>
2024-05-01 23:14:10 -0500

    feat: contact added

--- src/main.js
+++ src/main.js
@@ -6,16 +6,13 @@ import router from './router/index.js'
 // Add PrimeVue Components
 import Card         from 'primevue/card';
 import Button       from 'primevue/button';
-import SelectButton from 'primevue/selectbutton';
-import Sidebar      from 'primevue/sidebar';
-import Avatar       from 'primevue/avatar';
-import Menu         from 'primevue/menu';
-import MenuBar      from 'primevue/menubar';
-import Toolbar      from 'primevue/toolbar';
+import InputText from "primevue/inputtext";
+import 'primeicons/primeicons.css'
 
 createApp(App)
     .use(PrimeVue, { ripple: true })
     .use(router)
     .component('Card', Card)
     .component('Button', Button)
+    .component('pv-input-text', InputText)
     .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router';
 import LoginComponent from "../public/components/login.component.vue";
 import HomeComponent from "../public/components/home.component.vue";
 import RegisterCompanyComponent from "../public/components/register-company.component.vue";
+import Contact from "../public/components/contact/components/contact.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,7 +10,8 @@ const router = createRouter({
         {path: '/', redirect: '/login'},
         {path: '/login', component: LoginComponent, name: 'login'},
         {path: '/home', component: HomeComponent, name: 'home'},
-        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'}
+        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'},
+        {path:'/contact',component:Contact}
     ]
 });
 
