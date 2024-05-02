1762862e9f40f4e1457c1b9d4a8346e72d05ade3
LuisCh567 <s3bastianchavezguerrero@gmail.com>
2024-05-01 19:04:55 -0500

    feature: added toolbar and footer

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,15 @@ import {createRouter, createWebHistory} from 'vue-router';
 import LoginComponent from "../public/components/login.component.vue";
 import HomeComponent from "../public/components/home.component.vue";
 import RegisterCompanyComponent from "../public/components/register-company.component.vue";
-
+import ToolbarFooterComponent from "../public/components/toolbar-footer.component.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/login'},
         {path: '/login', component: LoginComponent, name: 'login'},
         {path: '/home', component: HomeComponent, name: 'home'},
-        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'}
+        {path: '/register-company', component: RegisterCompanyComponent, name: 'register-company'},
+        {path:'/toolbar-footer', component: ToolbarFooterComponent, name: 'toolbar-footer'},
     ]
 });
 
