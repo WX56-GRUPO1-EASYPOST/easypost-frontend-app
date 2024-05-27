1186367d5a9d32364b55fe6ab99af95baceeb9e6
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-05-27 01:39:38 -0500

    fix: requests list

--- src/core/enterprises/components/request.component.vue
+++ src/core/enterprises/components/request.component.vue
@@ -1,31 +1,22 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import requestService from "../services/requestService.js";
 import authService from "../../../public/services/authService.js";
-const requests = ref([]);
-const clients = ref([]);
+
+const props = defineProps({
+  request: Object
+});
+
+const client = ref(null);
+
 const selectedRequest = ref(null);
 
 onMounted(async () => {
   try {
-    const currentUrl = window.location.href;
-    const segments = currentUrl.split('/');
-    const companyId = segments[segments.length - 1];
-
-    console.log(companyId);
-    // Obtener las solicitudes por ID de empresa
-    requests.value = await requestService.getRequestsByCompanyId(companyId);
-    console.log(requests.value[0]);
-
-    await Promise.all(requests.value.map(async request => {
-      const user = await authService.getUserById(request.cliente_id);
-      console.log(user.data);
-      clients.value[request.id] = user.data;
-      console.log(clients.value[request.id].name);
-    }));
-
+    const response = await authService.getUserById(props.request.cliente_id);
+    client.value = response.data;
+    console.log(client.value)
   } catch (error) {
-    console.error('Error al obtener las solicitudes:', error);
+    console.error('Error al obtener el cliente:', error);
   }
 });
 
@@ -46,18 +37,12 @@ const showDetails = (request) => {
   <div class="main-container">
     <pv-card class="list-request">
       <template #content>
-    <h1 style="text-align: center">Solicitudes</h1>
-    <div v-if="requests.length === 0">
-      <p>No requests found</p>
-    </div>
-    <div v-else>
-      <div v-for="request in requests" :key="request.id">
         <pv-card class="card-request">
           <template #content >
             <div class="request-info">
               <div>
                 <h4>{{ request.ubicacion.localidad}} - {{request.ubicacion.distrito}}</h4>
-                <p v-if="clients">Cliente: {{ clients[request.id]?.name }}</p>
+                <p v-if="client">Cliente: {{ client.name }}</p>
                 <p>Ubicación: {{ request.ubicacion.departamento }},{{ request.ubicacion.provincia }},{{ request.ubicacion.distrito }}</p>
               </div>
               <div>
@@ -70,9 +55,6 @@ const showDetails = (request) => {
 
           </template>
         </pv-card>
-        <!-- Mostrar otros detalles de la solicitud según sea necesario -->
-      </div>
-    </div>
       </template>
     </pv-card>
     <!-- Tarjeta de detalles -->
@@ -81,8 +63,8 @@ const showDetails = (request) => {
         <h2>Detalles de la Solicitud</h2>
         <h3>Datos del Cliente</h3>
         <ul>
-          <li>Nombres: {{ clients[selectedRequest.id].name }}</li>
-          <li>Email: {{ clients[selectedRequest.id].email }}</li>
+          <li>Nombres: {{ client.name }}</li>
+          <li>Email: {{ client.email }}</li>
         </ul>
         <h3>Ubicación</h3>
         <ul>
--- src/core/enterprises/components/requests-list.component.vue
+++ src/core/enterprises/components/requests-list.component.vue
@@ -1,10 +1,33 @@
 <script setup>
-import Request from "../components/request.component.vue"
+import { ref, onMounted } from 'vue';
+import Request from "../components/request.component.vue";
+import requestService from "../services/requestService.js";
+
+const requests = ref([]);
+
+onMounted(async () => {
+  try {
+
+    const company = JSON.parse(localStorage.getItem('user'));
+    const companyId = company.id;
+    console.log(companyId)
+    const response = await requestService.getRequestsByCompanyId(companyId);
+    console.log(response); // Imprime la respuesta completa
+    requests.value = response; // Accede directamente a response
+    console.log(requests.value);
+  } catch (error) {
+    console.error('Error al obtener las solicitudes:', error);
+  }
+});
 </script>
 
 <template>
-<!--LLamar a request component-->
-
+  <h1 style="text-align: center">Solicitudes</h1>
+  <pv-card>
+    <template #content>
+      <Request v-for="request in requests" :key="request.id" :request="request" />
+    </template>
+  </pv-card>
 </template>
 
 <style scoped>
